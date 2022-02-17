3952561178486b46ca812899dbeace4ed16b79fe
hummusbird <hummusbirb@gmail.com>
2022-02-17 11:31:18 +0000

    fix: stats

--- img.birb.cc/img/index.js
+++ img.birb.cc/img/index.js
@@ -1,10 +1,8 @@
 async function stats() {
-    let res = await fetch("https:/img.birb.cc/api/stats")
+    let res = await fetch("/api/stats")
     let parsed = await res.json()
     console.log(parsed)
     document.getElementById("users").innerHTML = parsed.users + " registered users"
     document.getElementById("files").innerHTML = parsed.files + " files uploaded"
     document.getElementById("gb").innerHTML = Math.round(parsed.bytes/1024/1024) + "mb used"
-
-
 }
