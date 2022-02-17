c3b4a0a28230830d18938d282418ca28b9a2187c
hummusbird <hummusbirb@gmail.com>
2022-02-17 23:45:43 +0000

    update: stats site

--- img.birb.cc/img/index.js
+++ img.birb.cc/img/index.js
@@ -1,8 +1,9 @@
 async function stats() {
-    let res = await fetch("/api/stats")
+    let res = await fetch("https://img.birb.cc/api/stats")
     let parsed = await res.json()
     console.log(parsed)
     document.getElementById("users").innerHTML = parsed.users + " registered users"
     document.getElementById("files").innerHTML = parsed.files + " files uploaded"
     document.getElementById("gb").innerHTML = Math.round(parsed.bytes/1024/1024) + "mb used"
-}
+    document.getElementById("time").innerHTML = Math.round((new Date().getTime() - Date.parse(parsed.newest)) / 1000 / 60) + " mins since last upload"
+ }
