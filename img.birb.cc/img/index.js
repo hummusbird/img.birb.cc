e612e1570f22caa9928da33adeb35aed424d82af
hummusbird <hummusbirb@gmail.com>
2022-02-17 11:18:49 +0000

    feat: site

---
+++ img.birb.cc/img/index.js
@@ -0,0 +1,10 @@
+async function stats() {
+    let res = await fetch("https:/img.birb.cc/api/stats")
+    let parsed = await res.json()
+    console.log(parsed)
+    document.getElementById("users").innerHTML = parsed.users + " registered users"
+    document.getElementById("files").innerHTML = parsed.files + " files uploaded"
+    document.getElementById("gb").innerHTML = Math.round(parsed.bytes/1024/1024) + "mb used"
+
+
+}
