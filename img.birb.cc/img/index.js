2ec0e1b3dfccace38ac8c8a7598db3c1fa35936c
hummusbird <hummusbirb@gmail.com>
2022-02-21 19:43:21 +0000

    add commas

--- img.birb.cc/img/index.js
+++ img.birb.cc/img/index.js
@@ -2,8 +2,12 @@ async function stats() {
     let res = await fetch("api/stats")
     let parsed = await res.json()
     console.log(parsed)
-    document.getElementById("users").innerHTML = parsed.users + " registered users"
-    document.getElementById("files").innerHTML = parsed.files + " files uploaded"
-    document.getElementById("gb").innerHTML = Math.round(parsed.bytes/1024/1024) + "mb used"
-    document.getElementById("time").innerHTML = Math.round((new Date().getTime() - Date.parse(parsed.newest)) / 1000 / 60) + " mins since last upload"
- }
+    document.getElementById("users").innerHTML = numberWithCommas(parsed.users) + " registered users"
+    document.getElementById("files").innerHTML = numberWithCommas(parsed.files) + " files uploaded"
+    document.getElementById("gb").innerHTML = numberWithCommas(Math.round(parsed.bytes/1024/1024)) + "mb used"
+    document.getElementById("time").innerHTML = numberWithCommas(Math.round((new Date().getTime() - Date.parse(parsed.newest)) / 1000 / 60)) + " mins since last upload"
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
