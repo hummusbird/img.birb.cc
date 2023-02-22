dd91b91429665c2254aaa2c56c7f6fed05ff913b
hummusbird <birb@birb.cc>
2023-02-22 10:30:42 +0000

    config init

--- img.birb.cc/config.cs
+++ img.birb.cc/config.cs
@@ -1,8 +1,51 @@
+using Newtonsoft.Json;
+
 public class Config
 {
-    public string? UserDBPath { get; init; }
-    public string? FileDBPath { get; init; }
+    private const string path = "config.json";
+
+    public string? UserDBPath { get; init; } = "user.json";
+    public string? FileDBPath { get; init; } = "img.json";
 
     private readonly string? salt;
     private List<String>? fileTypes;
+
+    public static Config Load()
+    {
+        Config loadConfig = new Config();
+
+        try
+        {
+            using (StreamReader SR = new StreamReader(path))
+            {
+                string json = SR.ReadToEnd();
+
+                loadConfig = JsonConvert.DeserializeObject<Config>(json)!;
+                loadConfig.Save();
+            }
+            Console.WriteLine($"Loaded configuration file...");
+        }
+        catch
+        {
+            Console.WriteLine($"Unable to load {path}");
+
+        }
+
+        if (!File.Exists(path))
+        {
+            loadConfig!.Save();
+        }
+
+        return loadConfig;
+    }
+
+    public void Save()
+    {
+
+    }
+
+    public void LoadAllowedFileTypes()
+    {
+
+    }
 }
