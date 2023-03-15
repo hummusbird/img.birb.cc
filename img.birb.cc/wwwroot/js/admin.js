5ea85dd1ae387e4a163f355928b855dd734b554c
hummusbird <birb@birb.cc>
2023-03-15 11:40:13 +0000

    admin: add login script

--- img.birb.cc/wwwroot/js/admin.js
+++ img.birb.cc/wwwroot/js/admin.js
@@ -1,4 +1,26 @@
 const url = window.location.host
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
+let usersout;
+
 document.title = url + " // dashboard"
+
+async function login() {
+    let formData = new FormData();
+    formData.append("api_key", document.getElementById("keybox").value)
+
+    fetch(`https://${url}/api/users`,
+        {
+            body: formData,
+            method: "post"
+        }).then(res => res.json())
+        .then(
+            data => {
+                usersout = data
+                document.getElementById("loginstat").innerHTML = "api key:"
+                document.getElementById("loginarea").style.display = "none"
+
+                console.log(usersout)
+            })
+        .catch(e => document.getElementById("loginstat").innerHTML = "failed to login")
+}
