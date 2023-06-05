636bfc35340eca1d5f0cfebd84ddc542583bd493
hummusbird <birb@birb.cc>
2023-06-05 11:05:33 +0100

    album: html & js, remove css

---
+++ img.birb.cc/wwwroot/js/album.js
@@ -0,0 +1,90 @@
+const url = window.location.host;
+const hash = window.location.pathname.replace("/album/", "");
+
+document.title = "album // " + hash; // todo: replace with album name
+
+let imgout;
+
+login();
+
+async function login() {
+    let formData = new FormData();
+    formData.append("api_key", document.getElementById("keybox").value)
+
+    fetch(`https://${url}/api/album/${hash}/images`,
+        {
+            body: formData,
+            method: "post"
+        }).then(res => res.json())
+        .then(
+            data => {
+                imgout = JSON.parse(data);
+                document.getElementById("bottombutton").style.display = "block";
+                document.getElementById("topbuttons").style.display = "initial";
+                document.getElementById("albumtitle").innerHTML = hash;
+                document.getElementById("loginarea").style.display = "none";
+            }
+        )
+        .catch(e => {
+            document.getElementById("loginstat").innerHTML = "private album"
+            document.getElementById("loginarea").style.display = "flex";
+        })
+        .then(e => { if (imgout) { displayPage() } })
+}
+
+function display(filename, timestamp) {
+    document.getElementById("next").style.display = "none"
+    document.getElementById("preview").style.display = "initial"
+    document.getElementById("copyurl").innerHTML = "copy URL"
+    document.getElementById("copyurl").setAttribute("onclick", "copyToClipboard(" + `"${(document.getElementById("showURL").checked ? "​" : "")}https://${url}/${filename}")`)
+
+    if (filename.endsWith(".mp4")) {
+        document.getElementById("preview_vid").src = `https://${url}/` + filename
+        document.getElementById("preview_vid").style.display = "initial"
+        document.getElementById("preview_img").style.display = "none"
+    }
+    else {
+        document.getElementById("preview_img").src = `https://${url}/` + filename
+        document.getElementById("preview_img").style.display = "initial"
+        document.getElementById("preview_vid").style.display = "none"
+    }
+
+    document.getElementById("preview_info").innerHTML = filename + " // " + timestamp.split("T")[0]
+
+    document.getElementById("delete").setAttribute("onclick", `delimg("${filename.substr(0, 8)}");`)
+
+    document.getElementById("preview").addEventListener("click", function (e) {
+        e = window.event || e;
+        if (this === e.target) {
+            closePreview();
+        }
+    });
+}
+
+function displayPage(x) {
+    var pagecount = parseInt(document.getElementById("pagenum").value)
+
+    if (pagecount + x > 0 && pagecount + x <= Math.ceil(imgout.length / 50)) {
+        pagecount += x
+        document.getElementById("pagenum").value = pagecount
+    }
+
+    document.getElementById("images").innerHTML = null
+
+    var pagelength = 50
+    var length = pagecount * pagelength > imgout.length ? imgout.length : pagecount * pagelength
+
+    for (var i = (pagecount - 1) * pagelength; i < length; i++) {
+        var item = imgout[imgout.length - i - 1].endsWith(".mp4") ? document.createElement("video") : document.createElement("img");
+        item.src = `https://${url}/` + imgout[imgout.length - i - 1]
+        item.setAttribute("onclick", `display("${imgout[imgout.length - i - 1]}","${imgout[imgout.length - i - 1]["timestamp"]}")`)
+        document.getElementById("images").appendChild(item)
+    }
+}
+
+function closePreview() {
+    document.getElementById("next").style.display = "initial"
+    document.getElementById("preview").style.display = "none"
+
+    document.getElementById("preview_vid").src = ""
+}
